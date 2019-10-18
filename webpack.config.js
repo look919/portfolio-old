328b959d251f2091e2078d60a704f8551d4f2784
Wirkus Tomasz <wirkus919@gmail.com>
2019-10-18 13:35:23 +0200

    setup webpack production:

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,54 @@
 const path = require('path')
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-module.exports = {
-    entry: './src/app.js',
-    output: {
-        path: path.join(__dirname, 'public'),
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [{
-            loader: 'babel-loader',
-            test: /\.js$/,
-            exclude: /node_modules/
-        }, {
-            test: /\.s?css$/,
-            use: [
-                'style-loader',
-                'css-loader',
-                'sass-loader'
-            ]
-        }, {
-            test: /\.(png|svg|jpg|gif)$/,
-            use: [
-                'file-loader',
-            ]
-        }, {
-            test: /\.svg$/,
-            use: ['@svgr/webpack'],
+module.exports = (env) => {
+    const isProduction = env === 'production';
+    const CSSExtract = new ExtractTextPlugin('styles.css');
+
+    return{
+        entry: './src/app.js',
+        output: {
+            path: path.join(__dirname, 'public'),
+            filename: 'bundle.js'
         },
-    ]},
-    devtool: 'cheap-module-eval-source-map',
-    devServer: {
-        contentBase: path.join(__dirname, 'public'),
-        historyApiFallback: true
+        module: {
+            rules: [{
+                loader: 'babel-loader',
+                test: /\.js$/,
+                exclude: /node_modules/
+            }, {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    'file-loader',
+                ]
+            }, {
+                test: /\.s?css$/,
+                use: CSSExtract.extract({
+                  use: [
+                    {
+                      loader: 'css-loader',
+                      options: {
+                        sourceMap: true
+                      }
+                    },
+                    {
+                      loader: 'sass-loader',
+                      options: {
+                        sourceMap: true
+                      }
+                    }
+                  ]
+                })
+            }
+        ]},
+        plugins: [
+            CSSExtract
+        ],
+        devtool: isProduction ? 'source-map' : 'inline-source-map',
+        devServer: {
+            contentBase: path.join(__dirname, 'public'),
+            historyApiFallback: true
+        }
     }
+    
 }
