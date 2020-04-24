4c4535db4353e5e0904d91bf94552dc2b62468d4
Tomasz Wirkus <wirkus919@gmail.com>
2020-04-24 11:08:00 +0200

    exchange projects images for another with exact dimensions

--- src/components/SingleProject.js
+++ src/components/SingleProject.js
@@ -1,39 +1,39 @@
-import React from 'react';
+import React from "react";
 
 const SingleProject = ({ project, side }) => {
   return (
-    <figure className='project'>
-      <div className='project__info'>
-        <h2 className='project__title'>{project.title}</h2>
-        <a href={project.github} className='project__link' target='_blanc'>
+    <figure className="project">
+      <div className="project__info">
+        <h2 className="project__title">{project.title}</h2>
+        <a href={project.github} className="project__link" target="_blanc">
           Github
         </a>
-        <a href={project.url} className='project__link' target='_blanc'>
+        <a href={project.url} className="project__link" target="_blanc">
           Online
         </a>
       </div>
 
-      <div className='project__techs'>
+      <div className="project__techs">
         <img
-          className='project__img'
+          className="project__img"
           src={require(`../img/projects/${project.img}`)}
-          alt='project'
+          alt="project"
         />
 
-        <div className='project__technologies'>
+        <div className="project__technologies">
           {project.imgHover.map((el) => {
             return (
               <img
                 src={require(`../img/techs/${el}`)}
-                className='project__technologies__imgHover'
+                className="project__technologies__imgHover"
                 key={el}
-                alt='project technlogies'
+                alt="project technlogies"
               />
             );
           })}
         </div>
         {project.development && (
-          <div className='project__development'>In development</div>
+          <div className="project__development">In development</div>
         )}
       </div>
     </figure>
