3053287fac52769fe65ebdf8110ff2efa81ab00a
Tomasz Wirkus <wirkus919@gmail.com>
2020-04-24 12:56:49 +0200

    Changes in project page and modal for laptop and mobile view

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,13 +1,13 @@
-import React, { Component } from 'react';
-import 'react-responsive-carousel/lib/styles/carousel.min.css';
-import { Carousel } from 'react-responsive-carousel';
-import SingleProject from './SingleProject';
-const projects = require('../projects.json');
+import React, { Component } from "react";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from "react-responsive-carousel";
+import SingleProject from "./SingleProject";
+const projects = require("../projects.json");
 
 export default class DemoCarousel extends Component {
   render() {
     return (
-      <Carousel className='project' showThumbs={false}>
+      <Carousel showThumbs={false}>
         {projects.map((proj) => (
           <SingleProject key={proj} project={proj} />
         ))}
