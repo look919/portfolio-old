0a38904ff5eaca2fcf21b3aebed5bfa5c5fe37d9
Wirkus Tomasz <wirkus919@gmail.com>
2020-06-18 11:49:52 +0200

    Autoredirect to https via node

--- server/server.js
+++ server/server.js
@@ -7,7 +7,13 @@ const port = process.env.PORT || 3000;
 
 app.use(express.static(publicPath));
 app.use(enforce.HTTPS({ trustProtoHeader: true }));
-
+app.use(function (req, res, next) {
+  if (req.secure) {
+    next();
+  } else {
+    res.redirect('https://' + req.headers.host + req.url);
+  }
+});
 app.get('*', (req, res) => {
   res.sendFile(path.join(publicPath, 'index.html'));
 });
