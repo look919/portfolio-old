4f9bc40de6948e8eae0be08099f3a68afaa62854
Wirkus Tomasz <wirkus919@gmail.com>
2020-07-13 10:07:52 +0200

    Added https redirect package

--- server/server.js
+++ server/server.js
@@ -2,18 +2,12 @@ const path = require('path');
 const express = require('express');
 const app = express();
 const publicPath = path.join(__dirname, '..', 'public');
-const enforce = require('express-sslify');
+const sslRedirect = require('heroku-ssl-redirect');
 const port = process.env.PORT || 3000;
 
 app.use(express.static(publicPath));
-app.use(enforce.HTTPS({ trustProtoHeader: true }));
-app.use(function (req, res, next) {
-  if (req.secure) {
-    next();
-  } else {
-    res.redirect('https://' + req.headers.host + req.url);
-  }
-});
+app.use(sslRedirect());
+
 app.get('*', (req, res) => {
   res.sendFile(path.join(publicPath, 'index.html'));
 });
