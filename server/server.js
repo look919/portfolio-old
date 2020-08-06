897779ed067c1292a1dda590fa507fe9c50e2c5c
Wirkus Tomasz <wirkus919@gmail.com>
2020-08-06 22:21:26 +0200

    Diffrent approach to redirecting to https

--- server/server.js
+++ server/server.js
@@ -2,16 +2,19 @@ const path = require('path');
 const express = require('express');
 const app = express();
 const publicPath = path.join(__dirname, '..', 'public');
-const sslRedirect = require('heroku-ssl-redirect');
 const port = process.env.PORT || 3000;
 
 app.use(express.static(publicPath));
-app.use(sslRedirect());
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(publicPath, 'index.html'));
 });
 
-app.listen(port, () => {
-  console.log('Server is up!');
+//redirect to https
+app.use(function (req, res, next) {
+  if (req.secure) {
+    next();
+  } else {
+    res.redirect('https://' + req.headers.host + req.url); // request was via http, so redirect to https
+  }
 });
