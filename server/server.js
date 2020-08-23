8c482d16a27eb7beb4853b5be557eaafa217e848
Wirkus Tomasz <wirkus919@gmail.com>
2020-08-23 15:44:05 +0200

    Updated projects list

--- server/server.js
+++ server/server.js
@@ -2,13 +2,8 @@ const path = require('path');
 const express = require('express');
 const app = express();
 const publicPath = path.join(__dirname, '..', 'public');
-const port = process.env.PORT || 3000;
 
 app.use(express.static(publicPath));
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(publicPath, 'index.html'));
-});
 
 //redirect to https
 app.use(function (req, res, next) {
@@ -17,4 +12,8 @@ app.use(function (req, res, next) {
   } else {
     res.redirect('https://' + req.headers.host + req.url); // request was via http, so redirect to https
   }
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(publicPath, 'index.html'));
 });
